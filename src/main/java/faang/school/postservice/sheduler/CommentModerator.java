7b72a7aefbcb36ef685dc1b6e88ce074778aa297
Sergey Zhitenev <serzhitenev@yandex.ru>
2024-12-04 20:43:37 +0300

    fix: удалил @Async

--- src/main/java/faang/school/postservice/sheduler/CommentModerator.java
+++ src/main/java/faang/school/postservice/sheduler/CommentModerator.java
@@ -2,7 +2,6 @@ package faang.school.postservice.sheduler;
 
 import faang.school.postservice.service.CommentService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -12,7 +11,6 @@ public class CommentModerator {
     private final CommentService commentService;
 
     @Scheduled(cron = "${daily.cron.comment-moderation}")
-    @Async("customTaskExecutor")
     public void startModeration() {
         commentService.verifyComments();
     }
