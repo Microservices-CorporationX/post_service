7f639b7540ca200a60358379611ba7ef04770d9e
sars33 <sarsenyev@hotmail.com>
2024-12-06 23:21:06 +0200

    в PostController к настоящим методам, были добавлены анотации для Swagger и также к DTO обьектам.

--- src/main/java/faang/school/postservice/dto/post/PostCreateDto.java
+++ src/main/java/faang/school/postservice/dto/post/PostCreateDto.java
@@ -1,11 +1,16 @@
 package faang.school.postservice.dto.post;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.annotation.Nullable;
 import jakarta.validation.constraints.NotBlank;
 
+@Schema(description = "PostCreate Dto")
 public record PostCreateDto(
+        @Schema(description = "Контент поста")
         @NotBlank(message = "Поле \"content\" не заполнено") String content,
+        @Schema(description = "id автора")
         @Nullable Long authorId,
+        @Schema(description = "id проэкта")
         @Nullable Long projectId
 ) {
 
