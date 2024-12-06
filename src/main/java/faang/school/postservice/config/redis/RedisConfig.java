78fd1c9db965c31ceed09be438c48de6fef7cffa
Юрий <angelbes777@gmail.com>
2024-12-06 15:08:39 +0300

    Fix

--- src/main/java/faang/school/postservice/config/redis/RedisConfig.java
+++ src/main/java/faang/school/postservice/config/redis/RedisConfig.java
@@ -2,6 +2,7 @@ package faang.school.postservice.config.redis;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
@@ -11,6 +12,7 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisConfig {
     @Bean
+    @Primary
     public RedisTemplate<String, ?> redisTemplateWithJson(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, ?> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(connectionFactory);
