862b478860e1644965856231d12dca6ee2914339
Vakarash <max.vackarash@gmail.com>
2024-12-08 21:41:48 +0300

    Фикс неправильной аннотации на паблишере

--- src/main/java/faang/school/postservice/publisher/LikeEventPublisher.java
+++ src/main/java/faang/school/postservice/publisher/LikeEventPublisher.java
@@ -3,10 +3,10 @@ package faang.school.postservice.publisher;
 import faang.school.postservice.dto.like.LikeEvent;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
+@Component
 @RequiredArgsConstructor
-@Service
 public class LikeEventPublisher {
 
     private final RedisTemplate<String, Object> redisTemplate;
