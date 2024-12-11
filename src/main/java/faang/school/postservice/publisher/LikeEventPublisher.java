440e2bdfeb92261c9654ca0817d1a2ed707afabc
Vakarash <max.vackarash@gmail.com>
2024-12-11 14:36:30 +0300

    фикс топика в паблишере

--- src/main/java/faang/school/postservice/publisher/LikeEventPublisher.java
+++ src/main/java/faang/school/postservice/publisher/LikeEventPublisher.java
@@ -2,6 +2,7 @@ package faang.school.postservice.publisher;
 
 import faang.school.postservice.dto.like.LikeEvent;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
@@ -11,7 +12,10 @@ public class LikeEventPublisher {
 
     private final RedisTemplate<String, Object> redisTemplate;
 
+    @Value("${spring.data.redis.channels.like-event-topic}")
+    private String likeEventTopic;
+
     public void publish(LikeEvent event) {
-        redisTemplate.convertAndSend("likeEventTopic", event);
+        redisTemplate.convertAndSend(likeEventTopic, event);
     }
 }
