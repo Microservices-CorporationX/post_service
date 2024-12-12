7919929662646fe6cf06a5b9607de862528d529f
Юрий <angelbes777@gmail.com>
2024-12-12 14:31:49 +0300

    Update Helper.java

--- src/main/java/faang/school/postservice/utils/Helper.java
+++ src/main/java/faang/school/postservice/utils/Helper.java
@@ -3,8 +3,10 @@ package faang.school.postservice.utils;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class Helper {
@@ -14,6 +16,7 @@ public class Helper {
         try {
             return objectMapper.writeValueAsString(object);
         } catch (JsonProcessingException e) {
+            log.error("Error serializing object to JSON", e);
             throw new RuntimeException("Error serializing object to JSON", e);
         }
     }
