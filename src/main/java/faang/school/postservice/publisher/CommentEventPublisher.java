0edf179be12a2f9ce8f4f1fca3ae21f2f0a77e46
Andrew <andrew@MPro.local>
2024-12-22 12:59:07 +0700

    Feature: Comment Analytics : Post Service (Conflicts)
    Resolves comments

--- src/main/java/faang/school/postservice/publisher/CommentEventPublisher.java
+++ src/main/java/faang/school/postservice/publisher/CommentEventPublisher.java
@@ -3,18 +3,19 @@ package faang.school.postservice.publisher;
 import faang.school.postservice.config.redis.RedisProperties;
 import faang.school.postservice.event.CommentEvent;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.retry.annotation.Backoff;
 import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class CommentEventPublisher implements Publisher<CommentEvent> {
     private final RedisTemplate<String, Object> redisTemplate;
     private final RedisProperties redisProperties;
 
-    @Override
     @Retryable(
             retryFor = Exception.class,
             maxAttemptsExpression = "@retryProperties.maxAttempts",
@@ -23,7 +24,8 @@ public class CommentEventPublisher implements Publisher<CommentEvent> {
                     multiplierExpression = "@retryProperties.multiplier"
                     )
     )
-    public void publish(Object event) {
+    @Override
+    public void publish(CommentEvent event) {
         log.info("Publishing comment event: {} to channel: {}", event, redisProperties.getChannel().getCommentChannel());
         redisTemplate.convertAndSend(redisProperties.getChannel().getCommentChannel(), event);
     }
