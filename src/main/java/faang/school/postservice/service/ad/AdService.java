b8f00b92dc11a45c1e42140f92309f05e86efe7b
AnnaAplavina <59871896+AnnaAplavina@users.noreply.github.com>
2024-12-29 14:44:08 +0300

    BJS2-41428 - event type

--- src/main/java/faang/school/postservice/service/ad/AdService.java
+++ src/main/java/faang/school/postservice/service/ad/AdService.java
@@ -2,6 +2,7 @@ package faang.school.postservice.service.ad;
 
 
 import faang.school.postservice.dto.analytics.AnalyticsEventDto;
+import faang.school.postservice.model.event.EventType;
 import faang.school.postservice.publisher.ad.AdBoughtEventPublisher;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,7 +15,8 @@ public class AdService {
     private final AdBoughtEventPublisher adBoughtEventPublisher;
 
     public void buyAdd(long postId) {
-        AnalyticsEventDto adBoughtEvent = new AnalyticsEventDto(1L, 1L, -1, LocalDateTime.now());
+        AnalyticsEventDto adBoughtEvent =
+                new AnalyticsEventDto(1L, 1L, EventType.AD_BOUGHT_EVENT.ordinal(), LocalDateTime.now());
         adBoughtEventPublisher.publish(adBoughtEvent);
     }
 }
