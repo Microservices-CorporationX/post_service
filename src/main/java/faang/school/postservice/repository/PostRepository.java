b3c04aa0003991c13fb28de295f42c3568be1db0
Denis Levkhov <levkhovde@yandex.ru>
2025-01-04 05:51:39 +0300

    BJS2-41049: After merge fix

--- src/main/java/faang/school/postservice/repository/PostRepository.java
+++ src/main/java/faang/school/postservice/repository/PostRepository.java
@@ -1,17 +1,15 @@
 package faang.school.postservice.repository;
 
-import faang.school.postservice.dto.AuthorPostCount;
 import faang.school.postservice.model.Post;
-import jakarta.persistence.EntityNotFoundException;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Map;
 
 @Repository
-public interface PostRepository extends JpaRepository<Post, Long> {
+public interface PostRepository extends JpaRepository<Post, Long>, JpaSpecificationExecutor<Post> {
 
     List<Post> findByAuthorId(long authorId);
 
