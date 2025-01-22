dcab325eaa22d1483aa3656064f32142cf223c27
Eugene <@evgeniy_kim>
2025-01-22 10:50:23 +0500

    BJS2-52827 | Система постов

--- src/main/java/faang/school/postservice/dto/posts/PostSaveDto.java
+++ src/main/java/faang/school/postservice/dto/posts/PostSaveDto.java
@@ -1,8 +1,10 @@
 package faang.school.postservice.dto.posts;
 
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class PostSaveDto {
     private Long authorId;
     private Long projectId;
