25b949183062734113ded020981f45d354f90c2a
Roman Nifontov <romannif@mail.ru>
2025-01-23 23:03:12 +0500

    начал news feed

---
+++ src/main/java/faang/school/postservice/model/redis/PostRedis.java
@@ -0,0 +1,29 @@
+package faang.school.postservice.model.redis;
+
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.TimeToLive;
+
+import java.util.SortedSet;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@RedisHash("Post")
+public class PostRedis {
+
+    @Id
+    private Long id;
+    @TimeToLive
+    private Long expirationInSeconds;
+
+    private String content;
+
+
+
+}
