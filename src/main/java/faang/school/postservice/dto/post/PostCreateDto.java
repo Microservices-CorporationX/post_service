8af83ebe38d2ec1a5dcc67feafa0c0b35e4242e7
Stanislav <stasikbygaev@gmail.com>
2025-01-23 22:33:44 +0500

    Тесты к сервису

--- src/main/java/faang/school/postservice/dto/post/PostCreateDto.java
+++ src/main/java/faang/school/postservice/dto/post/PostCreateDto.java
@@ -3,10 +3,14 @@ package faang.school.postservice.dto.post;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class PostCreateDto {
     @NotBlank
     @Size(max = 4096)
