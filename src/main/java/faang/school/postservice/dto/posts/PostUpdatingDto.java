1a4ea0b69350244255abf5a270f3471920874fa2
az3l1t <eternity_cr9p@mail.ru>
2025-01-23 09:49:48 +0300

    исправил 1.4

--- src/main/java/faang/school/postservice/dto/posts/PostUpdatingDto.java
+++ src/main/java/faang/school/postservice/dto/posts/PostUpdatingDto.java
@@ -2,9 +2,8 @@ package faang.school.postservice.dto.posts;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Positive;
-import lombok.NonNull;
 
 public record PostUpdatingDto(
-        @NonNull @Positive Long postId,
+        @Positive Long postId,
         @NotBlank String updatingContent
 ) {}
