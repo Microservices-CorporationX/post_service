2877969e2ba8a6a3c8190914cc60b18c202d38e7
AesaQ <ilyaovchinnikov2095@gmail.com>
2025-01-24 12:16:33 +0500

    added commentLikeCount

--- src/main/java/faang/school/postservice/dto/comment/CommentResponse.java
+++ src/main/java/faang/school/postservice/dto/comment/CommentResponse.java
@@ -1,10 +1,8 @@
 package faang.school.postservice.dto.comment;
 
-/**
- * DTO for {@link faang.school.postservice.model.Comment}
- */
-public record CommentResponse(Long id,
-                              String content,
-                              Long authorId,
-                              Long postId) {
+public record CommentResponse (Long id,
+                               String content,
+                               Long authorId,
+                               Long postId,
+                               Integer likeCount) {
 }
