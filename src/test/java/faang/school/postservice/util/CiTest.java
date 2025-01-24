ecc98a926cb40559b02f6ebf2a87d89ea85bca3d
Leonid Zhuravel <zhuravel_leonid@yahoo.com>
2025-01-24 18:16:35 +0200

    added CI test

---
+++ src/test/java/faang/school/postservice/util/CiTest.java
@@ -0,0 +1,15 @@
+package faang.school.postservice.util;
+
+import org.junit.jupiter.api.Test;
+
+public class CiTest {
+    @Test
+    void placeholderTest() {
+        assert true;
+    }
+
+    @Test
+    void failingTest() {
+        assert false;
+    }
+}
