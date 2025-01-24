5ef3f3b4353cb319fcfeebf7537bbb1b980189f4
zubanyszarylkasynov <zubanyszarylkasyn@gmail.com>
2025-01-24 11:48:34 +0500

    feat(55555): TTL to caches

--- src/main/java/faang/school/postservice/config/redis/RedisConfig.java
+++ src/main/java/faang/school/postservice/config/redis/RedisConfig.java
@@ -3,8 +3,12 @@ package faang.school.postservice.config.redis;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -12,6 +16,8 @@ import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.GenericToStringSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
 
 @Configuration
 @RequiredArgsConstructor
@@ -25,6 +31,8 @@ public class RedisConfig {
 
     @Value("${spring.data.redis.channels.ban_user_channel.name}")
     private String userBanChannelTopic;
+
+    @Value("${spring.data.redis.cache.time-to-live:2}")
 
     @Bean
     public JedisConnectionFactory jedisConnectionFactory() {
@@ -48,6 +56,17 @@ public class RedisConfig {
         template.setKeySerializer(new GenericToStringSerializer<>(Long.class));
         template.setValueSerializer(new GenericJackson2JsonRedisSerializer(objectMapper));
         return template;
+    }
+
+    @Bean
+    public CacheManager cacheManager() {
+        RedisCacheConfiguration cacheConfig = RedisCacheConfiguration
+                .defaultCacheConfig()
+                .entryTtl(Duration.ofDays(10));
+
+        return RedisCacheManager.builder(jedisConnectionFactory())
+                .cacheDefaults(cacheConfig)
+                .build();
     }
 
     @Bean
