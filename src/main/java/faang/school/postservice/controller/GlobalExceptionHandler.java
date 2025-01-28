cdb8a1019ac6bf5121567931954e1435d5ddf3ad
AesaQ <ilyaovchinnikov2095@gmail.com>
2025-01-28 11:49:37 +0500

    added GlobalExceptionHandler

---
+++ src/main/java/faang/school/postservice/controller/GlobalExceptionHandler.java
@@ -0,0 +1,56 @@
+package faang.school.postservice.controller;
+
+import faang.school.postservice.exceptions.ErrorResponse;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleMethodArgumentNotValidException(MethodArgumentNotValidException e, WebRequest request) {
+        List<Map<String, String>> fieldErrors = e.getBindingResult().getFieldErrors().stream().map(fieldError -> Map.of(
+              "field", fieldError.getField(),
+              "message", Objects.requireNonNull(fieldError.getDefaultMessage())
+            )).toList();
+
+        log.error("MethodArgumentNotValidException: ", e);
+        return ErrorResponse.builder()
+                .path(request.getContextPath())
+                .details(fieldErrors)
+                .message(e.getMessage())
+                .build();
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleIllegalArgumentException(IllegalArgumentException e, WebRequest request) {
+        log.error("IllegalArgumentException: ", e);
+        return ErrorResponse.builder()
+                .path(request.getContextPath())
+                .message(e.getMessage())
+                .build();
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handleEntityNotFoundException(EntityNotFoundException e, WebRequest request) {
+        log.error("EntityNotFoundException: ", e);
+        return ErrorResponse.builder()
+                .path(request.getContextPath())
+                .message(e.getMessage())
+                .build();
+    }
+}
