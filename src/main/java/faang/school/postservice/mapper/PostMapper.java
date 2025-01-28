ca3258ad1036a65575334195f10c0ce102b7d780
Shakhzod Kamolov <shaxzod.kamolov@keysoft.uz>
2025-01-28 18:09:09 +0500

    Minor fix

--- src/main/java/faang/school/postservice/mapper/PostMapper.java
+++ src/main/java/faang/school/postservice/mapper/PostMapper.java
@@ -3,7 +3,6 @@ package faang.school.postservice.mapper;
 import faang.school.postservice.dto.post.PostDto;
 import faang.school.postservice.model.Post;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 
 @Mapper(componentModel = "spring",
@@ -12,11 +11,5 @@ public interface PostMapper {
 
     PostDto toDto(Post post);
 
-    @Mapping(target = "likes", ignore = true)
-    @Mapping(target = "comments", ignore = true)
-    @Mapping(target = "albums", ignore = true)
-    @Mapping(target = "ad", ignore = true)
-    @Mapping(target = "resources", ignore = true)
-    @Mapping(target = "scheduledAt", ignore = true)
     Post toEntity(PostDto post);
 }
