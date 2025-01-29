de19d8c44debb9425e8f78e2ca44147bc7a58318
Aleksi Basilashvili <lucheidobra@mail.ru>
2025-01-29 18:50:58 +0300

    Added feedHeater

--- src/main/java/faang/school/postservice/controller/newsfeed/FeedController.java
+++ src/main/java/faang/school/postservice/controller/newsfeed/FeedController.java
@@ -3,12 +3,17 @@ package faang.school.postservice.controller.newsfeed;
 import faang.school.postservice.config.context.UserContext;
 import faang.school.postservice.dto.newsfeed.FeedDto;
 import faang.school.postservice.service.newsfeed.FeedService;
+import faang.school.postservice.utils.FeedHeater;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/feed")
@@ -16,8 +21,9 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class FeedController {
 
-    public final UserContext userContext;
+    private final UserContext userContext;
     private final FeedService feedService;
+    private final FeedHeater feedHeater;
 
     @GetMapping
     public FeedDto getFeed(@RequestParam(value = "postId", required = false) Long postId){
@@ -25,5 +31,11 @@ public class FeedController {
         Long currentUser = userContext.getUserId();
         String postIdsStr = (postId != null) ? postId.toString() : null;
         return feedService.getFeed(currentUser.toString(), postIdsStr);
+    }
+
+    @PostMapping("/heat")
+    public void heatCacheForSpecificUsers(@RequestBody List<Long> userIds) {
+        log.info("Received a request to heat the cache for specific users");
+        feedHeater.heatCacheForSpecificUsers(userIds);
     }
 }
