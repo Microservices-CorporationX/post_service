9f45845eb04cb331de9bb04099ef3f9f1d536438
Shakhzod Kamolov <shaxzod.kamolov@keysoft.uz>
2025-01-29 12:47:49 +0500

    Add Swagger UI with configs and controller swagger properties

---
+++ src/main/java/faang/school/postservice/config/context/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package faang.school.postservice.config.context;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Post Service API")
+                        .version("1.0")
+                        .description("API для управления постами и комментариями")
+                        .contact(new Contact()
+                                .name("FAANG student")
+                                .email("course@faang.school"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("https://www.apache.org/licenses/LICENSE-2.0")));
+    }
+}
