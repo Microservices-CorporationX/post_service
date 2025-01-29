970978f2a8530e9a3db383802d880b1951225a70
lcodio <lcodio@temp.com>
2025-01-29 13:15:56 +0300

    add some @ to dto

--- src/main/java/faang/school/postservice/dto/post/RequestPostDto.java
+++ src/main/java/faang/school/postservice/dto/post/RequestPostDto.java
@@ -1,6 +1,8 @@
 package faang.school.postservice.dto.post;
 
-import lombok.NonNull;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Positive;
 import lombok.Value;
 
 import java.time.LocalDateTime;
@@ -8,13 +10,18 @@ import java.time.LocalDateTime;
 @Value
 public class RequestPostDto {
     Long id;
-    @NonNull
+    @NotBlank(message = "Content shoudn't be blank")
     String content;
+    @Positive
     Long authorId;
+    @Positive
     Long projectId;
     boolean published;
+    @PastOrPresent
     LocalDateTime publishedAt;
     LocalDateTime scheduledAt;
+    @PastOrPresent
     LocalDateTime createdAt;
+    @PastOrPresent
     LocalDateTime updatedAt;
 }
