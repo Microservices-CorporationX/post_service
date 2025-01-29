4b6858b51ca222452872aae7a0de5cae268c83c6
Aleksandr Kozlov <vismut-888@yandex.ru>
2025-01-29 10:17:57 +0300

    BJS2-54103: Все пользователи, которым понравился пост или комментарий

--- src/main/java/faang/school/postservice/repository/LikeRepository.java
+++ src/main/java/faang/school/postservice/repository/LikeRepository.java
@@ -1,7 +1,18 @@
 package faang.school.postservice.repository;
 
 import faang.school.postservice.model.Like;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface LikeRepository extends CrudRepository<Like, Long> {
+
+    @Query("SELECT l FROM Like l WHERE l.post.id = :postId")
+    List<Like> findAllByPostId(long postId);
+
+    @Query("SELECT l FROM Like l WHERE l.comment.id = :commentId")
+    List<Like> findAllByCommentId(long commentId);
 }
--- src/main/java/faang/school/postservice/service/LikeService.java
+++ src/main/java/faang/school/postservice/service/LikeService.java
@@ -1,6 +1,8 @@
 package faang.school.postservice.service;
 
+import faang.school.postservice.client.UserServiceClient;
 import faang.school.postservice.dto.like.LikeDto;
+import faang.school.postservice.dto.user.UserDto;
 import faang.school.postservice.exception.EntityNotFoundException;
 import faang.school.postservice.mapper.LikeMapper;
 import faang.school.postservice.model.Like;
@@ -11,15 +13,20 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class LikeService {
+    private final static int BATCH_SIZE = 100;
 
     private final PostService postService;
     private final CommentService commentService;
 
     private final UserService userService;
+    private final UserServiceClient userServiceClient;
 
     private final LikeRepository likeRepository;
     private final LikeValidator likeValidator;
@@ -54,14 +61,44 @@ public class LikeService {
 
     @Transactional
     public void removeCommentLike(long idComment, LikeDto dto) {
-        userService.getUserDtoById(dto.userId()); //проверка наличия юзера - при необходимости возвращает объект
-        Like like = commentService.getCommentById(idComment).getLikes()
-                .stream()
-                .filter(user -> user.getUserId().equals(dto.userId()))
-                .findFirst()
-                .orElseThrow(() -> new EntityNotFoundException("Лайк к комментарию с ID " + idComment + " не найден"));
-
-        likeRepository.delete(like);
+//        userService.getUserDtoById(dto.userId()); //проверка наличия юзера - при необходимости возвращает объект
+//        Like like = commentService.getCommentById(idComment).getLikes()
+//                .stream()
+//                .filter(user -> user.getUserId().equals(dto.userId()))
+//                .findFirst()
+//                .orElseThrow(() -> new EntityNotFoundException("Лайк к комментарию с ID " + idComment + " не найден"));
+//
+//        likeRepository.delete(like);
     }
 
+    public List<UserDto> getAllUsersWhoLikedPost(Long postId) {
+        return getUserDto(likeRepository
+                .findAllByPostId(postId).stream()
+                .map(Like::getUserId)
+                .toList());
+    }
+
+    public List<UserDto> getAllUsersWhoLikedComment(Long commentId) {
+        return getUserDto(likeRepository
+                .findAllByCommentId(commentId).stream()
+                .map(Like::getUserId)
+                .toList());
+    }
+
+    private List<UserDto> getUserDto(List<Long> usersId) {
+        List<List<Long>> batches = new ArrayList<>();
+        for (int i = 0; i < usersId.size(); i += BATCH_SIZE) {
+            List<Long> batch = usersId.subList(i, Math.min(i + BATCH_SIZE, usersId.size()));
+            batches.add(batch);
+        }
+        return collectUsersDto(batches);
+    }
+
+    private List<UserDto> collectUsersDto(List<List<Long>> batches) {
+        List<UserDto> userDto = new ArrayList<>();
+        for (List<Long> batch : batches) {
+            userDto.addAll(userServiceClient.getUsersByIds(batch));
+        }
+        return userDto;
+    }
 }
