a26dc456b1aa5df4c3045a7fe03355ef59ed6835
Aleksi Basilashvili <lucheidobra@mail.ru>
2025-01-29 18:01:31 +0300

    Added getFeed method in Controller+Service.

--- src/main/java/faang/school/postservice/dto/newsfeed/FeedDto.java
+++ src/main/java/faang/school/postservice/dto/newsfeed/FeedDto.java
@@ -1,23 +1,20 @@
 package faang.school.postservice.dto.newsfeed;
 
+import faang.school.postservice.dto.PostDto;
+import faang.school.postservice.dto.user.UserDto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class FeedDto {
-    private Long userId;
-    private Long profilePicFileId;
-    private Long profilePicSmallFileId;
-
-    private Long postId;
-    private Long authorId;
-    private String content;
-    private LocalDateTime publishedAt;
+    private List<UserDto> userDtos;
+    private List<PostDto> posts;
 }
