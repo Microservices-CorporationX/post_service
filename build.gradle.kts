a4da29c43b5faf49ebcd5bab6d0047fcfb43aadf
Leonid Zhuravel <zhuravel_leonid@yahoo.com>
2025-01-30 09:00:00 +0200

    PR- related final fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -56,6 +56,11 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-params:5.9.2")
     testImplementation("org.assertj:assertj-core:3.24.2")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    /**
+     * AWS S3
+     */
+    implementation("software.amazon.awssdk:s3:2.17.0")
 }
 
 tasks.test {
