8c891040b08e1e8cd903467b5b6e33de123651a8
lcodio <lcodio@temp.com>
2025-01-30 22:01:58 +0300

    init

--- src/main/java/faang/school/postservice/repository/PostRepository.java
+++ src/main/java/faang/school/postservice/repository/PostRepository.java
@@ -28,5 +28,4 @@ public interface PostRepository extends JpaRepository<Post, Long> {
             "FROM Post p " +
             "WHERE p.published = false AND p.deleted = false AND p.scheduledAt <= CURRENT_TIMESTAMP")
     List<Post> findReadyToPublish();
-
 }
