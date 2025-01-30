18fa1bda73c70e5ab65ebd811e7f0b029fa6adff
Ilya Vysokolyan <rd2d@list.ru>
2025-01-30 18:50:09 +0300

    BJS2-54105: GlobalExc

--- src/main/java/faang/school/postservice/exception/GlobalExceptionHandler.java
+++ src/main/java/faang/school/postservice/exception/GlobalExceptionHandler.java
@@ -1,18 +1,66 @@
 package faang.school.postservice.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
-    @ExceptionHandler(BusinessException.class)
-    public ResponseEntity<String> handleBusinessException(BusinessException ex) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleException(BusinessException ex) {
+        return new ErrorResponse(
+                ex.getMessage(),
+                "Business",
+                HttpStatus.BAD_REQUEST
+        );
     }
 
-    @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity<String> handleEntityNotFoundException(EntityNotFoundException ex) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleException(DataValidationException ex) {
+        return new ErrorResponse(
+                ex.getMessage(),
+                "DataValidation",
+                HttpStatus.BAD_REQUEST
+        );
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handleException(EntityNotFoundException ex) {
+        return new ErrorResponse(
+                ex.getMessage(),
+                "Entity not found",
+                HttpStatus.NOT_FOUND
+        );
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleException(
+            MissingServletRequestParameterException ex
+    ) {
+        return new ErrorResponse(
+                "Query параметр не был найден",
+                ex.getMessage(),
+                HttpStatus.BAD_REQUEST
+        );
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handleException(Exception ex) {
+        log.error(ex.getMessage(), ex);
+        return new ErrorResponse(
+                ex.getMessage(),
+                "Непредвиденная ошибка сервера",
+                HttpStatus.INTERNAL_SERVER_ERROR
+        );
     }
 }
