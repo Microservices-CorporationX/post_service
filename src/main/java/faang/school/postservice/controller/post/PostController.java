5fc8dad9486deb63be4dacc06298f2764ffb2a5f
lcodio <lcodio@temp.com>
2025-01-31 21:14:16 +0300

    fix

--- src/main/java/faang/school/postservice/controller/post/PostController.java
+++ src/main/java/faang/school/postservice/controller/post/PostController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -59,7 +59,7 @@ public class PostController {
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
-    @PutMapping("/update/{post-id}")
+    @PatchMapping("/update/{post-id}")
     public ResponseEntity<Void> updateSubProject(
             @PathVariable("post-id") final Long postId,
             @RequestBody @Valid RequestPostDto requestPostDto) {
