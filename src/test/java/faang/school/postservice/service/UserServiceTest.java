e822917d89098f7fbb4cbb818c189bdd968f827c
Natalya Vasileva <boryantheone228@yandex.ru>
2025-02-01 14:48:11 +0300

    BJS2-54109 Исправление тестов для UserService

--- src/test/java/faang/school/postservice/service/UserServiceTest.java
+++ src/test/java/faang/school/postservice/service/UserServiceTest.java
@@ -3,13 +3,17 @@ package faang.school.postservice.service;
 import faang.school.postservice.client.UserServiceClient;
 import faang.school.postservice.dto.user.UserDto;
 import feign.FeignException;
+import jakarta.persistence.EntityNotFoundException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 
@@ -19,18 +23,36 @@ class UserServiceTest {
     @Mock
     private UserServiceClient userServiceClient;
 
+    @InjectMocks
+    private UserService userService;
+
     @Test
     public void testGetUserIfUserExists() {
         UserDto userDto = UserDto.builder().build();
         when(userServiceClient.getUser(anyLong())).thenReturn(userDto);
 
-        assertNotNull(userServiceClient.getUser(anyLong()));
+        assertNotNull(userService.getUser(anyLong()));
     }
 
     @Test
     public void testGetUserIfUserNotExists() {
         when(userServiceClient.getUser(anyLong())).thenThrow(FeignException.FeignClientException.class);
 
-        assertThrows(FeignException.FeignClientException.class, () -> userServiceClient.getUser(anyLong()));
+        assertThrows(EntityNotFoundException.class, () -> userService.getUser(anyLong()));
+    }
+
+    @Test
+    public void testIsUserExistsIfUserExists() {
+        UserDto userDto = UserDto.builder().build();
+        when(userServiceClient.getUser(anyLong())).thenReturn(userDto);
+
+        assertTrue(userService.isUserExists(anyLong()));
+    }
+
+    @Test
+    public void testIsUserExistsIfUserNotExists() {
+        when(userServiceClient.getUser(anyLong())).thenThrow(FeignException.FeignClientException.class);
+
+        assertFalse(userService.isUserExists(anyLong()));
     }
 }
