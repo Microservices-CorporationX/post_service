307bfe1c30eb3f2ebc2377378136252846dc2950
Aleksandr Kuznetsov <adeskmath@gmail.com>
2025-02-01 14:56:31 +0300

    added: send event by chuncks / in batches

--- src/main/java/faang/school/postservice/producer/PostEventProducer.java
+++ src/main/java/faang/school/postservice/producer/PostEventProducer.java
@@ -2,8 +2,13 @@ package faang.school.postservice.producer;
 
 import faang.school.postservice.config.kafka.KafkaProperties;
 import faang.school.postservice.dto.event.PostEventDto;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
@@ -12,11 +17,51 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class PostEventProducer {
 
+  @Value("${newsfeed.subscribers.batch-size:10000}")
+  private int batchSize;
+
   private final KafkaTemplate<String, Object> kafkaTemplate;
   private final KafkaProperties kafkaProperties;
 
-  public void sendEvent(PostEventDto event) {
+  private final ExecutorService cachedThreadPool;
+
+  public void sendEvent(PostEventDto postEvent) {
     log.info("Sending New Post Event with Followers to Kafka");
-    kafkaTemplate.send(kafkaProperties.getPostsTopic(), event);
+
+    List<PostEventDto> batches = splitIntoBatches(postEvent);
+
+    List<CompletableFuture<Void>> futures = batches.stream()
+            .map(event -> CompletableFuture.runAsync(() -> kafkaTemplate.send(kafkaProperties.getPostsTopic(), event), cachedThreadPool))
+        .toList();
+
+    CompletableFuture.allOf(futures.toArray(new CompletableFuture[0]))
+        .thenRun(() -> log.info("all messages delivered to Kafka"));
   }
+
+  private List<PostEventDto> splitIntoBatches(PostEventDto postEventDto) {
+    Long postId = postEventDto.getPosId();
+    List<Long> followers = postEventDto.getFollowers();
+
+    int followersCount = followers.size();
+
+    int batchNumbs = (followersCount + batchSize - 1) / batchSize;
+
+    List<PostEventDto> postEventBatches = new ArrayList<>();
+
+    for (int i = 0; i < batchNumbs; i++) {
+      int start = i * batchSize;
+      int end = Math.min(followersCount, (i + 1) * batchSize);
+
+      List<Long> followersSublist = followers.subList(start, end);
+
+      PostEventDto postEventPart = PostEventDto.builder()
+          .posId(postId)
+          .followers(followersSublist)
+          .build();
+
+      postEventBatches.add(postEventPart);
+    }
+    return postEventBatches;
+  }
+
 }
