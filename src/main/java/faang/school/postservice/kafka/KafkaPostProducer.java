a130f82191a4452dc2a722b1d0d716bea906e2ab
Ruslan G. <marlinco263@gmail.com>
2025-02-01 20:46:20 +0100

    BJS2-42296 completed

--- src/main/java/faang/school/postservice/kafka/KafkaPostProducer.java
+++ src/main/java/faang/school/postservice/kafka/KafkaPostProducer.java
@@ -2,6 +2,7 @@ package faang.school.postservice.kafka;
 
 import faang.school.postservice.dto.post.PostCreatedEvent;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +13,9 @@ import java.util.List;
 @Log4j2
 public class KafkaPostProducer {
 
+    @Value(value = "${spring.kafka.topicName}")
+    private String topicName;
+
     private final KafkaTemplate<String, PostCreatedEvent> kafkaTemplate;
 
     public KafkaPostProducer(KafkaTemplate<String, PostCreatedEvent> kafkaTemplate) {
@@ -20,6 +24,6 @@ public class KafkaPostProducer {
 
     public void sendPostCreatedEvent(Long postId, Long authorId, List<Long> subscriberIds) {
         PostCreatedEvent event = new PostCreatedEvent(postId, authorId, subscriberIds, Instant.now());
-        kafkaTemplate.send("post-created", event);
+        kafkaTemplate.send(topicName, event);
     }
 }
